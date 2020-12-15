76f45340b7fa0f1e72571a004eff166b6616adb8
pogov <maciejpogorzelski@MacBook-Pro-Maciej.local>
2020-12-16 00:28:28 +0100

    added pagination

--- controllers/ShoesController.js
+++ controllers/ShoesController.js
@@ -2,9 +2,31 @@ const Shoe = require("../models/Shoe");
 
 const shoes = {
   getallShoes: async (req, res) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const result = {};
+
+    if (endIndex < (await Shoe.countDocuments().exec())) {
+      result.next = {
+        page: page + 1,
+        limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      result.previous = {
+        page: page - 1,
+        limit,
+      };
+    }
+
     try {
-      const shoes = await Shoe.find();
-      res.json(shoes);
+      result.shoes = await Shoe.find().limit(limit).skip(startIndex).exec();
+      res.status(201).json(result);
     } catch (err) {
       res.status(404).json({ message: err.message });
     }
