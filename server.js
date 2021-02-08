e51d46592710094d5953b4cd0ecc9bef79a6bf24
pogov <maciejpogorzelski@MacBook-Pro-Maciej.local>
2021-02-08 22:49:04 +0100

    cors change 5

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors());
 app.use(express.json({ extended: false }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "*");
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "GET");
