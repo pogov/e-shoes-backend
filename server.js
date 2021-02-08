436bc9e954adc2e2ada8b324ce66ee3f61eab400
pogov <maciejpogorzelski@MacBook-Pro-Maciej.local>
2021-02-08 18:44:26 +0100

    cors change

--- server.js
+++ server.js
@@ -24,6 +24,17 @@ const app = express();
 
 app.use(cors(corsOptions));
 app.use(express.json({ extended: false }));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use("/", routes);
 
 app.listen(process.env.PORT || port, () =>
